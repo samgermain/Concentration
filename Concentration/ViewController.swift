1274c54deb767d5bd103b7c01aeb31d5c24d3539
Sam <sam.germain@usask.ca>
2019-10-20 17:35:27 -0600

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Samuel Germain on 2019-10-20.
+//  Copyright © 2019 Sam G. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
